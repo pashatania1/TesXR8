62b4b1f292932c1edbe8d64c88f7c4888977c28d
Pashatania Fitri Indah Lestari <87978506+pashatania1@users.noreply.github.com>
2024-08-02 14:58:43 +0700

    Add files via upload

---
+++ Tambah.java
@@ -0,0 +1,8 @@
+public class Tambah {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 222;
+        int hasil = a+b;
+        System.out.println("Hasil dari " + a + " ditambah " + b + " adalah " + hasil);
+    }
+}
